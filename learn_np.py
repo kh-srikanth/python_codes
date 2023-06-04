c8afd20395c3fcd791e74fb61e772737babe3977
kh-srikanth <kh.srikanth@gmail.com>
2023-06-04 10:55:54 +0530

    Python practice
    (cherry picked from commit 623c288c9849aabc7a4f3f22e4af3e57628e6b74)

--- learn_np.py
+++ learn_np.py
@@ -1,18 +1,31 @@
 import numpy as np
-a = np.array([[1, 2, 3]
-             ,[4, 5, 6]
-             ,[7, 8, 9]])
-print(a)
-print(np.sqrt(a))
-print(a.sum(axis=1))
-print(a.reshape(1,9))
-print(a.size)
-print(a.shape)
+# a = np.array([[1, 2, 3]
+#              ,[4, 5, 6]
+#              ,[7, 8, 9]])
+# print(a)
+# print(np.sqrt(a))
+# print(a.sum(axis=1))
+# print(a.reshape(1,9))
+# print(a.size)
+# print(a.shape)
+#
+# b = np.array([[11,22,33]
+#              ,[44,55,66]
+#              ,[77,88,99]])
+# print('sum of a and b is:')
+# print(a + b)
+# print(b.dot(a))
+# print(a.dot(b))
 
-b = np.array([[11,22,33]
-             ,[44,55,66]
-             ,[77,88,99]])
-print('sum of a and b is:')
-print(a + b)
-print (b.dot(a))
-print (a.dot(b))
+c = np.arange(0,6).reshape(3,2)
+d = np.arange(6,12).reshape(3,2)
+print(c)
+print(d)
+##union two tables
+print(np.vstack((c,d)))
+##fulljoin two tables
+print(np.hstack((c,d)))
+aa = np.arange(30).reshape(2,15)
+print(aa)
+print(np.hsplit(aa,3))
+print(np.vsplit(aa,2))
---
+++ learn_np2.py
@@ -0,0 +1,7 @@
+import numpy as np
+a = np.arange(12).reshape(3,4)
+print(a)
+b = a > 4
+print(b)
+
+print(a[b])
